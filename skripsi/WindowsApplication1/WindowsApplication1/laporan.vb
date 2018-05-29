77e3e27c093e930dd7e660f885114bd6a4c6ef95
Ego Dafma Dasa <egodasa@gmail.com>
2018-05-29 22:01:51 +0700

    Report per day, month, year and custom time is created and finished

--- skripsi/WindowsApplication1/WindowsApplication1/laporan.vb
+++ skripsi/WindowsApplication1/WindowsApplication1/laporan.vb
@@ -18,7 +18,7 @@ Imports System
 Imports System.ComponentModel
 
 
-Public Class laporanberobatperhari
+Public Class laporan
     Inherits ReportClass
     
     Public Sub New()
@@ -27,7 +27,7 @@ Public Class laporanberobatperhari
     
     Public Overrides Property ResourceName() As String
         Get
-            Return "laporanberobatperhari.rpt"
+            Return "laporan.rpt"
         End Get
         Set
             'Do nothing
@@ -45,7 +45,7 @@ Public Class laporanberobatperhari
     
     Public Overrides Property FullResourceName() As String
         Get
-            Return "WindowsApplication1.laporanberobatperhari.rpt"
+            Return "WindowsApplication1.laporan.rpt"
         End Get
         Set
             'Do nothing
@@ -91,10 +91,18 @@ Public Class laporanberobatperhari
             Return Me.ReportDefinition.Sections(4)
         End Get
     End Property
+    
+    <Browsable(false),  _
+     DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
+    Public ReadOnly Property Parameter_judul() As CrystalDecisions.[Shared].IParameterField
+        Get
+            Return Me.DataDefinition.ParameterFields(0)
+        End Get
+    End Property
 End Class
 
 <System.Drawing.ToolboxBitmapAttribute(GetType(CrystalDecisions.[Shared].ExportOptions), "report.bmp")>  _
-Public Class Cachedlaporanberobatperhari
+Public Class Cachedlaporan
     Inherits Component
     Implements ICachedReport
     
@@ -136,7 +144,7 @@ Public Class Cachedlaporanberobatperhari
     End Property
     
     Public Overridable Function CreateReport() As CrystalDecisions.CrystalReports.Engine.ReportDocument Implements CrystalDecisions.ReportSource.ICachedReport.CreateReport
-        Dim rpt As laporanberobatperhari = New laporanberobatperhari()
+        Dim rpt As laporan = New laporan()
         rpt.Site = Me.Site
         Return rpt
     End Function
