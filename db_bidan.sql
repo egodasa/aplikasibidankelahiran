8bcf6b604a1e31ba4f749b4afad38a83ac2a0592
Ego Dafma Dasa <egodasa@gmail.com>
2018-06-01 09:53:11 +0700

    New export database with trigger included

--- db_bidan.sql
+++ db_bidan.sql
@@ -1,3 +1,15 @@
+-- Adminer 4.6.2 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP DATABASE IF EXISTS `db_bidan`;
+CREATE DATABASE `db_bidan` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `db_bidan`;
+
+
 DROP TABLE IF EXISTS `tbl_anc`;
 CREATE TABLE `tbl_anc` (
   `id_periksa` varchar(50) NOT NULL,
@@ -21,6 +33,11 @@ CREATE TABLE `tbl_asuhan_bayi` (
   PRIMARY KEY (`id_asuhan`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `tbl_asuhan_bayi` (`id_asuhan`, `nm_asuhan`) VALUES
+(1,	'Inisasi Menyusu Dini (IMD) dalam 1 jam pertama'),
+(2,	'Suntikan Vitamin K1'),
+(3,	'Salep mata antibiotika profilaksis'),
+(4,	'Imunisasi HBO');
 
 DROP TABLE IF EXISTS `tbl_bayi_lahir`;
 CREATE TABLE `tbl_bayi_lahir` (
@@ -46,6 +63,11 @@ CREATE TABLE `tbl_jenis_obat` (
   PRIMARY KEY (`id_jobat`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `tbl_jenis_obat` (`id_jobat`, `nm_jobat`) VALUES
+(1,	'KB'),
+(2,	'Ringan'),
+(3,	'Keras'),
+(4,	'Suplemen');
 
 DROP TABLE IF EXISTS `tbl_kelahiran`;
 CREATE TABLE `tbl_kelahiran` (
@@ -76,6 +98,15 @@ CREATE TABLE `tbl_kondisi_lahir` (
   PRIMARY KEY (`id_kondisi`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `tbl_kondisi_lahir` (`id_kondisi`, `nm_kondisi`) VALUES
+(1,	'Segera menangis'),
+(2,	'Menangis beberapa saat'),
+(3,	'Tidak menangis'),
+(4,	'Seluruh tubuh kemerahan'),
+(5,	'Anggota gerak kebiruan'),
+(6,	'Seluruh tubuh biru'),
+(7,	'Kelainan bawaan'),
+(8,	'Meninggal');
 
 DROP TABLE IF EXISTS `tbl_obat`;
 CREATE TABLE `tbl_obat` (
@@ -88,6 +119,15 @@ CREATE TABLE `tbl_obat` (
   PRIMARY KEY (`id_obat`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `tbl_obat` (`id_obat`, `nm_obat`, `stok`, `status`, `id_jobat`, `hrg_obat`) VALUES
+(1,	'Kabe',	12,	'Aktif',	1,	10000),
+(2,	'Ringan',	2,	'Aktif',	2,	1800),
+(8,	'Suntik',	9,	'Aktif',	1,	9000),
+(9,	'Parasetamol',	10,	'Aktif',	2,	1100),
+(10,	'STM',	10,	'Aktif',	2,	1000),
+(11,	'Promag',	30,	'Aktif',	2,	2000),
+(12,	'Amoxilin',	40,	'Aktif',	2,	3000),
+(13,	'Vitamin C',	12,	'Aktif',	2,	12000);
 
 DROP TABLE IF EXISTS `tbl_pasien`;
 CREATE TABLE `tbl_pasien` (
@@ -100,6 +140,9 @@ CREATE TABLE `tbl_pasien` (
   PRIMARY KEY (`no_pasien`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `tbl_pasien` (`no_pasien`, `nm_pasien`, `tgl_lahir`, `jk`, `pekerjaan`, `alamat`) VALUES
+(4,	'Wanita Pasien',	'1971-05-28',	'Perempuan',	'Sebuah Pekerjaan',	'Alamat rumah contoh'),
+(5,	'Pasien Laki',	'1990-08-01',	'Laki-laki',	'Tukang Angkek',	'Perumahan ');
 
 DROP TABLE IF EXISTS `tbl_periksa`;
 CREATE TABLE `tbl_periksa` (
@@ -125,6 +168,9 @@ CREATE TABLE `tbl_periksa_kb` (
   PRIMARY KEY (`id_kb`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `tbl_periksa_kb` (`no_pasien`, `id_kb`, `tgl_periksa`, `nm_suami`, `anak_ke`, `haid_terakhir`, `berat_badan`, `tensi`) VALUES
+(4,	'300520181126079965',	'2018-05-30 04:26:18',	'231',	2,	'2018-05-30',	432,	'11'),
+(4,	'300520181131455272',	'2018-05-30 04:32:31',	'qwe',	2,	'2018-05-30',	12,	'12');
 
 DROP TABLE IF EXISTS `tbl_terapi`;
 CREATE TABLE `tbl_terapi` (
@@ -135,10 +181,29 @@ CREATE TABLE `tbl_terapi` (
   PRIMARY KEY (`id_terapi`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE VIEW `laporan_kb` AS select `a`.`no_pasien` AS `no_pasien`,`c`.`nm_pasien` AS `nm_pasien`,`c`.`tgl_lahir` AS `tgl_lahir`,`a`.`tgl_periksa` AS `tgl_periksa`,`a`.`anak_ke` AS `anak_ke`,`a`.`haid_terakhir` AS `haid_terakhir`,`a`.`berat_badan` AS `berat_badan`,`a`.`tensi` AS `tensi`,`b`.`nm_obat` AS `nm_obat` from ((`tbl_periksa_kb` `a` join `laporan_terapi` `b` on((`a`.`id_kb` = `b`.`id_periksa`))) join `tbl_pasien` `c` on((`a`.`no_pasien` = `c`.`no_pasien`)));
+INSERT INTO `tbl_terapi` (`id_terapi`, `id_obat`, `jumlah`, `id_periksa`) VALUES
+(41,	1,	1,	'300520181126079965'),
+(42,	1,	1,	'300520181131455272'),
+(44,	9,	10,	'3005181120014255'),
+(45,	2,	10,	'3005181122144613');
 
-CREATE VIEW `laporan_pemasukan` AS select `a`.`id_terapi` AS `id_terapi`,`a`.`id_periksa` AS `id_periksa`,`b`.`nm_obat` AS `nm_obat`,sum(`a`.`jumlah`) AS `jumlah`,sum((`a`.`jumlah` * `b`.`hrg_obat`)) AS `total_bayar` from (`tbl_terapi` `a` join `tbl_obat` `b` on((`a`.`id_obat` = `b`.`id_obat`))) group by `a`.`id_periksa`;
+DELIMITER ;;
 
-CREATE VIEW `laporan_terapi` AS select `a`.`id_periksa` AS `id_periksa`,`a`.`id_obat` AS `id_obat`,`b`.`nm_obat` AS `nm_obat`,`b`.`hrg_obat` AS `hrg_obat`,`a`.`jumlah` AS `jumlah`,(`a`.`jumlah` * `b`.`hrg_obat`) AS `total` from (`tbl_terapi` `a` join `tbl_obat` `b` on((`a`.`id_obat` = `b`.`id_obat`)));
+CREATE TRIGGER `kurangi_stok` AFTER INSERT ON `tbl_terapi` FOR EACH ROW
+update tbl_obat set stok = stok - new.jumlah where id_obat = new.id_obat;;
 
--- 2018-05-30 04:58:47
+CREATE TRIGGER `reset_stok` AFTER DELETE ON `tbl_terapi` FOR EACH ROW
+update tbl_obat set stok = stok + OLD.jumlah where id_obat = OLD.id_obat;;
+
+DELIMITER ;
+
+DROP TABLE IF EXISTS `laporan_kb`;
+CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `laporan_kb` AS select `a`.`no_pasien` AS `no_pasien`,`c`.`nm_pasien` AS `nm_pasien`,`c`.`tgl_lahir` AS `tgl_lahir`,`a`.`tgl_periksa` AS `tgl_periksa`,`a`.`anak_ke` AS `anak_ke`,`a`.`haid_terakhir` AS `haid_terakhir`,`a`.`berat_badan` AS `berat_badan`,`a`.`tensi` AS `tensi`,`b`.`nm_obat` AS `nm_obat` from ((`tbl_periksa_kb` `a` join `laporan_terapi` `b` on((`a`.`id_kb` = `b`.`id_periksa`))) join `tbl_pasien` `c` on((`a`.`no_pasien` = `c`.`no_pasien`)));
+
+DROP TABLE IF EXISTS `laporan_pemasukan`;
+CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `laporan_pemasukan` AS select `a`.`id_terapi` AS `id_terapi`,`a`.`id_periksa` AS `id_periksa`,`b`.`nm_obat` AS `nm_obat`,sum(`a`.`jumlah`) AS `jumlah`,sum((`a`.`jumlah` * `b`.`hrg_obat`)) AS `total_bayar` from (`tbl_terapi` `a` join `tbl_obat` `b` on((`a`.`id_obat` = `b`.`id_obat`))) group by `a`.`id_periksa`;
+
+DROP TABLE IF EXISTS `laporan_terapi`;
+CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `laporan_terapi` AS select `a`.`id_periksa` AS `id_periksa`,`a`.`id_obat` AS `id_obat`,`b`.`nm_obat` AS `nm_obat`,`b`.`hrg_obat` AS `hrg_obat`,`a`.`jumlah` AS `jumlah`,(`a`.`jumlah` * `b`.`hrg_obat`) AS `total` from (`tbl_terapi` `a` join `tbl_obat` `b` on((`a`.`id_obat` = `b`.`id_obat`)));
+
+-- 2018-06-01 02:48:13
