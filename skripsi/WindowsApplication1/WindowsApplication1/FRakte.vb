ebf1f6592a471549761b0cdd67afc4dd95f5d7ec
Ego Dafma Dasa <egodasa@gmail.com>
2018-06-11 12:03:02 +0700

    Perbaikan akte kelahiran sudah diterapkan.

--- skripsi/WindowsApplication1/WindowsApplication1/FRakte.vb
+++ skripsi/WindowsApplication1/WindowsApplication1/FRakte.vb
@@ -4,7 +4,7 @@ Public Class FRakte
     Dim CR As New ReportDocument()
     Private Sub FRakte_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         CR.Load(_DIR & "suratketerangankelahiran.rpt")
-        CR.SetParameterValue("id_kelahiran", Fdaftar_kelahiran.DGkelahiran.CurrentRow.Cells("Id_Kelahiran").Value)
+        CR.RecordSelectionFormula = "{daftar_kelahiran1.Id_Kelahiran} = '" & Fdaftar_kelahiran.DGkelahiran.CurrentRow.Cells("Id_Kelahiran").Value & "'"
         CRakte.ReportSource = CR
     End Sub
 End Class
